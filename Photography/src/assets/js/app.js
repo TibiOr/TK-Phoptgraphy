64e842697d8ebadc11f8fc6ca28ff46b5038751f
Orban Cristina <CristinaOrban@bitbucket.org>
2019-09-21 15:27:32 +0300

    Tibi project

--- Photography/src/assets/js/app.js
+++ Photography/src/assets/js/app.js
@@ -18,3 +18,40 @@ $(document).foundation();
 
 var navigation = require('../js/navigation');
 navigation.Open();
+
+function generateHtml([h,v]) {
+    var  nr = randomNumber(43);
+    return `
+        <div class="item ih${h} iv${v}">
+            <img src="../assets/img/${nr}.jpg" alt="img${nr}" />
+            <div class="view-overlay">
+                <button class="button view-button">View</button>
+            </div
+        </div>
+    `;
+}
+
+function randomNumber(limit) {
+    return Math.floor(Math.random()*limit) + 1;
+}
+
+$(document).ready(function() {
+    $('button.view-button').on('click', function(){ 
+        const image = $(this).parents('.item').find('img').clone();
+        $('.reveal').append(image);
+        $('.reveal-overlay').show();
+        $('.reveal').show();
+    });
+
+    $('.reveal').on('click','.close-button', function () { 
+        $('.reveal-overlay').hide();
+        $('.reveal').hide();
+        $('.reveal>img').remove();
+    });
+})
+
+const digits = Array.from({ length:43}, () => [randomNumber(4), randomNumber(4)]);
+
+const html = digits.map(generateHtml);
+
+$('.gallery').html(html);
